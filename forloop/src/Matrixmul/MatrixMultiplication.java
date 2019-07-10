eaff933c3170ad2d119016f951c4e0bca01f8f12
Lenovo <pritammgaikwad@gmail.com>
2019-07-10 18:34:00 +0530

    Matrixmultiplication1

--- forloop/src/Matrixmul/MatrixMultiplication.java
+++ forloop/src/Matrixmul/MatrixMultiplication.java
@@ -1,7 +1,9 @@
-public class MatrixMulti {
+package Matrixmul;
+
+public class MatrixMultiplication {
     public static void main(String args[]){
 
-    int a[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int a[][]={{1,2,3},{4,5,6},{7,8,9}};
 
         int b[][]={{9,8,7},{6,5,4},{3,2,1}};
 
@@ -20,5 +22,5 @@ public class MatrixMulti {
             System.out.println();
         }
     }
-}
 
+}
